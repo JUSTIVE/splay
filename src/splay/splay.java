90d57131066eaa044108ea6a9d1b53f35423ddda
Ben <ben399399@naver.com>
2015-05-09 02:41:11 +0900

    circle created

--- src/splay/splay.java
+++ src/splay/splay.java
@@ -4,56 +4,67 @@ import java.awt.*;
 
 import javax.swing.*;
 
+
 public class splay {
+	
 	public static void main(String []args){
 		
 		int width=600;
 		int height=800;
 		
 		JFrame frame=new JFrame();
-		frame.setBounds(300, 300, width, height);
+		frame.setSize(width+15, height+39);
+		frame.setLocation(15,39);
 		frame.setUndecorated(false);
-		frame.getContentPane().setLayout(new FlowLayout(FlowLayout.LEFT,0,0));
+		frame.getContentPane().setLayout(null);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	
-		
+			
 		JPanel paper=new JPanel();
 		paper.setVisible(true);
-		paper.setPreferredSize(new Dimension(width,height));
+		paper.setSize(width, height);
 		paper.setBackground(Color.decode("#e5e5e5"));
-		paper.setLayout(new FlowLayout(FlowLayout.LEFT,0,0));
+		paper.setLayout(null);
+		paper.setLocation(0, 0);
 		
 		JPanel plain=new JPanel();
 		plain.setVisible(true);
-		plain.setPreferredSize(new Dimension(570,560));
 		plain.setBackground(Color.decode("#ffffff"));
-		plain.setLayout(new FlowLayout(FlowLayout.LEFT,0,0));
-		plain.setLocation(new Point(15,225));
+		plain.setSize(570,560);
+		plain.setLocation(15, 227);
 		
-		JButton exit=new JButton();
-		
+		Circle exit=new Circle();
+		exit.setVisible(true);
+		exit.setLocation(495,165);
+		exit.setSize(87,87);
+				
 		JPanel notibar=new JPanel();
 		notibar.setVisible(true);
-		notibar.setPreferredSize(new Dimension(width,20));
 		notibar.setBackground(new Color(0,0,0,77));
+		notibar.setSize(width, 20);
+		notibar.setLocation(0, 0);
 		
 		JPanel title=new JPanel();
-		title.setPreferredSize(new Dimension(width,210));
-		title.setBackground(Color.decode("#3f51b5"));
 		title.setVisible(true);
-		title.setLayout(new FlowLayout(FlowLayout.LEFT,0,0));
+		title.setBackground(Color.decode("#3f51b5"));
+		title.setLayout(null);
+		title.setSize(width, 210);
+		title.setLocation(0,0);
 		
 		Label titlename=new Label("S play");
 		titlename.setVisible(true);
-		titlename.setFont(new Font("¸¼Àº °íµñ",Font.BOLD,35));
+		//titlename.setFont(new Font("Adobe °íµñ Std",Font.BOLD,42));
+		titlename.setFont(new Font("Adobe °íµñ Std B",Font.BOLD,42));
 		titlename.setForeground(Color.decode("#FFFFFF"));
+		titlename.setBounds(75, 130, 150, 70);
 		
-		//titlename.setAlignmentX(135);
-		//titlename.setAlignmentY(75);
 		title.add(notibar);
 		title.add(titlename);
+		
+		paper.add(exit);
 		paper.add(title);
 		paper.add(plain);
+		
+		
 		frame.add(paper);
 		frame.setVisible(true);
 	}
