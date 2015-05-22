148baeb09b8f41c75c964d0bfcf5abfd396e0494
JUSTIVE <ben399399@naver.com>
2015-05-22 18:19:46 +0900

    test
    added?

--- src/splay/Circle.java
+++ src/splay/Circle.java
@@ -12,15 +12,34 @@ public class Circle extends JPanel{
 	{
 		this.color=color;
 	}
+	public void SetColor(Color c)
+	{
+		this.color=c;
+		//repaint();
+	}
 	public void paintComponent(Graphics graphics)
 	{
-		super.paintComponent(graphics);
+		//super.paintComponent(graphics);
 		graphics.fillOval(0,0, 87, 87);
-		Graphics2D ga=(Graphics2D)graphics;
-		ga.setRenderingHint( RenderingHints.KEY_ANTIALIASING,RenderingHints.VALUE_ANTIALIAS_ON);
-	    ga.setRenderingHint(RenderingHints.KEY_RENDERING,RenderingHints.VALUE_RENDER_QUALITY);
+		//Graphics2D ga=(Graphics2D)graphics;
+		//ga.setRenderingHint( RenderingHints.KEY_ANTIALIASING,RenderingHints.VALUE_ANTIALIAS_ON);
+	    //ga.setRenderingHint(RenderingHints.KEY_RENDERING,RenderingHints.VALUE_RENDER_QUALITY);
 		setForeground(color);
-		setBackground(new Color(00,00,00,00));
-		super.paintComponent(ga);
+		setBackground(new Color(0,0,0,0));
+		//super.paintComponent(ga);
+	}
+}
+class CirclePanel extends JPanel
+{
+	public CirclePanel(int width,int height){
+		setVisible(true);
+		setSize(width,height+2);
+		setLayout(null);
+		setBackground(new Color(0,0,0,0));
+		Circle shadow=new Circle(new Color(0,0,0,77));
+		shadow.setVisible(true);
+		shadow.setSize(width,height);
+		shadow.setLocation(0,2);
+		super.add(shadow,-1,0);
 	}
 }
--- src/splay/Render.java
+++ src/splay/Render.java
@@ -8,34 +8,37 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 import java.awt.Image;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
 public class Render {
 	public JFrame mainframe;
 	public int width=600;
 	public int height=800;
 	public int level;
+	public int posX,posY;
+	int Px,Py;
 	Render(){
 		level=0;
 		mainframe=new JFrame();
+		posX=16;
+		posY=39;
 	}
-	public void draw()
+	//inital start screen
+	public void drawlogos()
 	{
-		switch(level)
-		{
-		case 0:
-			drawmain();
-			break;
-		}
+		//mainframe.
 	}
 	public void drawmain()
 	{
-		mainframe.setUndecorated(false);
+		mainframe.setUndecorated(true);
 		mainframe.setResizable(false);
 		mainframe.setVisible(true);
 		mainframe.getContentPane().setLayout(null);
-		mainframe.setSize(width+6, height+29);
-		mainframe.setLocation(15,39);
+		mainframe.setSize(width, height);
+		//mainframe.setSize(width+6, height+29);
+		mainframe.setLocation(0,0);
 		mainframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
 		PaperPanel titlecontainer=new PaperPanel(width,210);
 		titlecontainer.setLocation(0, 0);
 			
@@ -58,18 +61,20 @@ public class Render {
 		
 		CirclePanel exitcontainer=new CirclePanel(87, 87);
 		exitcontainer.setLocation(495, 165);
-		exitcontainer.setBackground(new Color(00,00,00,00));
 		
 		Circle exit=new Circle(Color.decode("#ef5350"));
 		exit.setVisible(true);
 		exit.setLayout(null);
+		exit.addMouseListener(new ClickcolorCircle());
 		exit.setLocation(0,0);
 		exit.setSize(87,87);
 		
 		IconPanel exiticon=new IconPanel(".\\images\\exit.png",45,45);
 		exiticon.setLocation(22,22);
-		
+			
 		JPanel notibar=new JPanel();
+		notibar.addMouseListener(new Moveframe());
+		notibar.addMouseMotionListener(new Movingframe());
 		notibar.setVisible(true);
 		notibar.setBackground(new Color(0,0,0,77));
 		notibar.setSize(width, 20);
@@ -88,6 +93,7 @@ public class Render {
 		JPanel playbutton=new JPanel();
 		playbutton.setVisible(true);
 		playbutton.setLayout(null);
+		playbutton.addMouseListener(new Clickcolor());
 		playbutton.setBackground(Color.decode("#5c6bc0"));
 		playbutton.setSize(105, 105);
 		playbutton.setLocation(0,0);
@@ -101,6 +107,7 @@ public class Render {
 		JPanel creditbutton=new JPanel();
 		creditbutton.setVisible(true);
 		creditbutton.setLayout(null);
+		creditbutton.addMouseListener(new Clickcolor());
 		creditbutton.setBackground(Color.decode("#5c6bc0"));
 		creditbutton.setSize(105, 105);
 		creditbutton.setLocation(0,0);
@@ -114,6 +121,7 @@ public class Render {
 		JPanel aboutbutton=new JPanel();
 		aboutbutton.setVisible(true);
 		aboutbutton.setLayout(null);
+		aboutbutton.addMouseListener(new Clickcolor());
 		aboutbutton.setBackground(Color.decode("#5c6bc0"));
 		aboutbutton.setSize(105, 105);
 		aboutbutton.setLocation(0,0);
@@ -123,8 +131,7 @@ public class Render {
 		
 		JLabel titlename=new JLabel("S play");
 		titlename.setVisible(true);
-		//titlename.setFont(new Font("Adobe °íµñ Std",Font.BOLD,42));
-		titlename.setFont(new Font("Adobe °íµñ Std B",Font.PLAIN,42));
+		titlename.setFont(new Font("HY°­B",Font.PLAIN,42));
 		titlename.setForeground(Color.decode("#FFFFFF"));
 		titlename.setBounds(75, 130, 150, 70);
 				
@@ -146,8 +153,7 @@ public class Render {
 		aboutbutton.add(abouticon);
 		
 		plaincontainer.add(plain);
-		
-		
+				
 		exitcontainer.add(exit,0,0);
 		exit.add(exiticon,2,0);
 				
@@ -155,7 +161,58 @@ public class Render {
 		paper.add(plaincontainer);
 		paper.add(titlecontainer);
 		mainframe.add(paper);
-		mainframe.setVisible(true);
 	}
-	
+	class Clickcolor extends MouseAdapter{
+		@Override
+		public void mousePressed(MouseEvent e)
+		{
+			JPanel play=(JPanel)e.getSource();
+			play.setBackground(Color.decode("#3949AB"));
+		}
+		
+		@Override
+		public void mouseReleased(MouseEvent e)
+		{
+			JPanel play=(JPanel)e.getSource();
+			play.setBackground(Color.decode("#5c6bc0"));
+		}
+	}
+	class ClickcolorCircle extends MouseAdapter{
+		@Override
+		public void mousePressed(MouseEvent e)
+		{
+			if(Math.sqrt(Math.pow(e.getX()-(44),2)+Math.pow(e.getY()-(44), 2))<43)
+			{
+				Circle btn=(Circle)e.getSource();
+				btn.SetColor(Color.decode("#C62828"));
+				btn.repaint();
+				//TODO:: clear background glitter
+			}
+		}
+		public void mouseReleased(MouseEvent e)
+		{
+			if(Math.sqrt(Math.pow(e.getX()-(44),2)+Math.pow(e.getY()-(44), 2))<43)
+			{
+				Circle btn=(Circle)e.getSource();
+				btn.SetColor(Color.decode("#ef5350"));
+				btn.setBackground(new Color(0,0,0,0));
+				mainframe.dispose();
+				System.exit(1);
+			}
+		}
+	}
+	class Moveframe extends MouseAdapter{
+		@Override
+		public void mousePressed(MouseEvent e)
+		{
+			Px=e.getX();
+			Py=e.getY();
+		}	
+	}
+	class Movingframe extends MouseMotionAdapter{
+		public void mouseDragged(MouseEvent evt)
+		{
+			mainframe.setLocation(mainframe.getLocation().x+evt.getX()-Px,mainframe.getLocation().y+evt.getY()-Py);
+		}
+	}
 }
